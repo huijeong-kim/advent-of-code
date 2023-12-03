12039c2331eb98e180510c14241ab16d5fea3a10
Huijeong Kim <herehuijeong@gmail.com>
2023-12-03 16:36:21 +0900

    Solves 2023 DAY3

---
+++ advent-of-code-2023/src/bin/day3.rs
@@ -0,0 +1,156 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn test_input() -> Vec<String> {
+    let input_str = "467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..";
+    input_str
+        .lines()
+        .into_iter()
+        .map(|line| line.to_string())
+        .collect()
+}
+
+fn day3_input() -> Vec<String> {
+    let file = File::open("inputs/day3.txt").unwrap();
+    let lines = BufReader::new(file).lines();
+    lines.into_iter().map(|line| line.unwrap()).collect()
+}
+
+fn prev_loc(line: usize) -> usize {
+    if line == 0 {
+        0
+    } else {
+        line - 1
+    }
+}
+fn next_loc(line: usize) -> usize {
+    line + 1
+}
+fn between(target: usize, min: usize, max: usize) -> bool {
+    min <= target && target <= max
+}
+
+#[derive(Default, Debug)]
+struct Number {
+    value: String,
+    line: usize,
+    start: usize,
+    end: usize,
+    adjacent_symbols: String,
+}
+impl Number {
+    pub fn new(c: char, loc: (usize, usize)) -> Self {
+        Self {
+            value: c.to_string(),
+            line: loc.0,
+            start: loc.1,
+            end: loc.1,
+            adjacent_symbols: String::new(),
+        }
+    }
+    fn can_be_added(&self, loc: (usize, usize)) -> bool {
+        self.line == loc.0 && self.end + 1 == loc.1
+    }
+    fn add(&mut self, c: char, loc: (usize, usize)) {
+        assert_eq!(self.end + 1, loc.1);
+        self.end += 1;
+        self.value.push(c);
+    }
+
+    fn update_adjacent(&mut self, symbols: &mut Vec<Symbol>) {
+        for s in symbols.iter_mut() {
+            if between(s.loc.0, prev_loc(self.line), next_loc(self.line)) &&
+                between(s.loc.1, prev_loc(self.start), next_loc(self.end))
+            {
+                self.adjacent_symbols.push(s.value);
+                s.adjacent_numbers.push(self.value());
+            }
+        }
+    }
+
+    fn has_adjacent(&self) -> bool {
+        self.adjacent_symbols.len() != 0
+    }
+    fn value(&self) -> u32 {
+        self.value.parse().unwrap()
+    }
+}
+
+#[derive(Debug)]
+struct Symbol {
+    value: char,
+    loc: (usize, usize),
+    adjacent_numbers: Vec<u32>,
+}
+
+fn main() {
+    let input = day3_input();
+    let (numbers, symbols) = get(&input);
+
+    let result = part1(&numbers);
+    println!("result: {}", result.iter().sum::<u32>());
+
+    let result = part2(&symbols);
+    println!("result: {:?}", result.iter().sum::<u32>());
+}
+
+fn get(input: &Vec<String>) -> (Vec<Number>, Vec<Symbol>) {
+    let mut symbols = Vec::new();
+    let mut numbers: Vec<Number> = Vec::new();
+
+    input.iter().enumerate().for_each(|(idx, value)| {
+        value.chars().enumerate().for_each(|(c_idx, c)| {
+            if c.is_numeric() {
+                if numbers.last().is_some() && numbers.last().unwrap().can_be_added((idx, c_idx)) {
+                    numbers.last_mut().unwrap().add(c, (idx, c_idx));
+                } else {
+                    numbers.push(Number::new(c, (idx, c_idx)));
+                }
+            }
+            else if c != '.' {
+                symbols.push(Symbol {
+                    value: c,
+                    loc: (idx, c_idx),
+                    adjacent_numbers: Vec::new(),
+                });
+            }
+        })
+    });
+
+    numbers.iter_mut()
+        .for_each(|num| {
+            num.update_adjacent(&mut symbols);
+        });
+
+    (numbers, symbols)
+}
+
+fn part1(numbers: &Vec<Number>) -> Vec<u32> {
+    numbers
+        .iter()
+        .map(|num| {
+            if num.has_adjacent() {
+                num.value()
+            } else {
+                0
+            }
+        })
+        .collect()
+}
+
+fn part2(symbols: &Vec<Symbol>) -> Vec<u32> {
+    symbols
+        .iter()
+        .filter(|s| s.value == '*' && s.adjacent_numbers.len() == 2)
+        .map(|g| g.adjacent_numbers.iter().fold(1, |acc, &n| acc * n))
+        .collect()
+}
