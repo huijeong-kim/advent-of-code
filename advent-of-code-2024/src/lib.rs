2b875bba60bdd62b1b7634df5b7b81af5e80e190
huijeong-kim <herehuijeong@gmail.com>
2024-12-02 21:14:42 +0900

    2024 Day 02

--- advent-of-code-2024/src/lib.rs
+++ advent-of-code-2024/src/lib.rs
@@ -8,3 +8,12 @@ pub fn read_from_file(filename: &str) -> Result<String, std::io::Error> {
 
     Ok(buffer)
 }
+
+pub fn is_same_vec(vec1: &Vec<i32>, vec2: &Vec<i32>) -> bool {
+    let matching = vec1
+        .iter()
+        .zip(vec2.iter())
+        .filter(|&(a, b)| a == b)
+        .count();
+    matching == vec1.len() && matching == vec2.len()
+}
